4eba2a8d9fbad281dfd8fcd3df23750ea531c378
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 16:11:40 +0530

    [Atleast one capital letters]

--- userReg.sh
+++ userReg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 shop -s extglob
 echo "Welcome to user registration"
+while [ true ]
+do
 #read -p "enter  username :  "  username
 #read -p "enter userlast name : " userlastname 
 #read -p "enter emil : " emil
@@ -11,7 +13,8 @@ pat2="^[a-z]{2,}[A-Z]{1}$"
 pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
 pat4=='^((\+){1}91){1}[ ]?[0-9]{10}$'
 pat5="^[a-zA-Z0-9]{8,}"
-if [[ $password =~ $pat5 ]]
+pat6="^([a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*)$"
+if [[ $password =~ $pat6 ]]
 then
 
 echo "valid password"
