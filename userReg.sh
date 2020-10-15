4411dd0bfd1281deb8061a7f09d0f227bf76ed0d
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 15:50:32 +0530

    [valid mobilenumber with country code]

--- userReg.sh
+++ userReg.sh
@@ -3,14 +3,17 @@ shop -s extglob
 echo "Welcome to user registration"
 #read -p "enter  username :  "  username
 #read -p "enter userlast name : " userlastname 
-read -p "enter emil : " emil
+#read -p "enter emil : " emil
+read -p "enter mobilenumber : " mobilenumber
 pat="^[A-Z]{1}[a-z]{2,}$"
 pat2="^[a-z]{2,}[A-Z]{1}$"
 pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
-if [[ $email =~ $pat3 ]]
+pat4=='^((\+){1}91){1}[ ]?[0-9]{10}$'
+
+if [[ $mobilenumber =~ $pat4 ]]
 then
 
-echo "valid email"
+echo "valid mobilenumber"
 
 else
 
