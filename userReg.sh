4a3125f863ce208ee1cc5a34e73265608250553e
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 07:39:00 +0530

    [Valid emil]

--- userReg.sh
+++ userReg.sh
@@ -2,13 +2,15 @@
 shop -s extglob
 echo "Welcome to user registration"
 #read -p "enter  username :  "  username
-read -p "enter userlast name : " userlastname 
+#read -p "enter userlast name : " userlastname 
+read -p "enter emil : " emil
 pat="^[A-Z]{1}[a-z]{2,}$"
-pat2=="^[a-z]{2,}[A-Z]{1}$"
-if [[ $userlastname =~ $pat2 ]]
+pat2="^[a-z]{2,}[A-Z]{1}$"
+pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
+if [[ $userlastname =~ $pat3 ]]
 then
 
-echo "valid userlastname"
+echo "valid email"
 
 else
 
