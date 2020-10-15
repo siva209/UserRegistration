fce330eb434c03f35d6a3e50ae191821ce7dadd8
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 07:29:09 +0530

    [Enter valid Last name start with capital letter]

--- userReg.sh
+++ userReg.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 shop -s extglob
 echo "Welcome to user registration"
-read -p "enter  username :  "  username
+#read -p "enter  username :  "  username
+read -p "enter userlast name : " userlastname 
 pat="^[A-Z]{1}[a-z]{2,}$"
-if [[ $username =~ $pat ]]
+pat2=="^[a-z]{2,}[A-Z]{1}$"
+if [[ $userlastname =~ $pat2 ]]
 then
 
-echo "valid username"
+echo "valid userlastname"
 
 else
 
