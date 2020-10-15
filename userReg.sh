065d9d881c160c84d0dfe46424f0949afc3ebe38
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 16:00:27 +0530

    [Password min 8 characters]

--- userReg.sh
+++ userReg.sh
@@ -4,16 +4,17 @@ echo "Welcome to user registration"
 #read -p "enter  username :  "  username
 #read -p "enter userlast name : " userlastname 
 #read -p "enter emil : " emil
-read -p "enter mobilenumber : " mobilenumber
+#read -p "enter mobilenumber : " mobilenumber
+read -p "enter  password : " password
 pat="^[A-Z]{1}[a-z]{2,}$"
 pat2="^[a-z]{2,}[A-Z]{1}$"
 pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
 pat4=='^((\+){1}91){1}[ ]?[0-9]{10}$'
-
-if [[ $mobilenumber =~ $pat4 ]]
+pat5="^[a-zA-Z0-9]{8,}"
+if [[ $password =~ $pat5 ]]
 then
 
-echo "valid mobilenumber"
+echo "valid password"
 
 else
 
