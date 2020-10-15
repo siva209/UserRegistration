13f586825d9e1489673a75528af0236deb15cfce
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-15 07:16:51 +0530

    [Enter valid first name with capital letter]

--- userReg.sh
+++ userReg.sh
@@ -1,1 +1,18 @@
-Welcome to regex
+#!/bin/bash
+shop -s extglob
+echo "Welcome to user registration"
+read -p "enter  username :  "  username
+pat="^[A-Z]{1}[a-z]{2,}$"
+if [[ $username =~ $pat ]]
+then
+
+echo "valid username"
+
+else
+
+echo "not valid "
+
+fi
+
+
+
